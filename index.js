38eadb00680bcb98fbc46c164cfd7b0312e0065f
aws1718casm <carlossm50@gmail.com>
2018-05-06 21:11:02 +0000

    update

--- index.js
+++ index.js
@@ -150,7 +150,10 @@ projects.connectDb((err) => {
         console.log("Could not connect with MongoDB");
         process.exit(1);
     }
-    users.connectDb((err) => {
+    app.listen(port, () => {
+            console.log("Server with GUI up and running!!");
+        });   
+    /*users.connectDb((err) => {
         if (err) {
             console.log("Could not connect with MongoDB");
             process.exit(1);
@@ -159,5 +162,5 @@ projects.connectDb((err) => {
             console.log("Server with GUI up and running!!");
         });   
         
-    });
+    });*/
 });
